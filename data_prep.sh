0be743d38d5cde83543651b4448498f8d441b627
Mohammed OE Abdallah <melsiddieg@users.noreply.github.com>
2023-02-09 00:50:25 -0800

    Update data_prep.sh

--- data_prep.sh
+++ data_prep.sh
@@ -1,6 +1,6 @@
 # for both GRCh37 and GRCh38
 ## restrict analysis to larger chromosomes
-awk '\$2 > 10000000 || \$1 ~/(M|MT)\$/ { print \$1"\t0\t"\$2 }' $fai | bgzip -c > cr.bed.gz
+awk '$2 > 10000000 || $1 ~/(M|MT)$/ { print $1"\t0\t"$2 }' $fai | bgzip -c > cr.bed.gz
 tabix cr.bed.gz
 
 
